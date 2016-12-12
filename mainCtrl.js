2cd3d832c9596b676e392618623bb5f4c2a5f7de
marcandy <marcandy.nj@gmail.com>
2016-12-11 20:38:30 -0700

    jquery hamberger menu

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,4 +1,18 @@
 angular.module('ecom')
   .controller('mainCtrl', function ($scope) {
-    $scope.test = 'working';
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "fast", function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+    });
+    });
+
+    $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+    });
+    });
   })
